cf8cc37c6b357e268168f29c711fc79ad55e0b4f
Aareef <93aareefabrar@gmail.com>
2018-05-10 02:26:54 +0600

    first thing

---
+++ app/home/home.js
@@ -0,0 +1,89 @@
+'use restrict';
+
+angular.module('webApp.home', ['ngRoute','firebase'])
+
+
+.config(['$routeProvider', function($routeProvider){
+    $routeProvider.when('/home', {
+        templateUrl: 'home/home.html',
+        controller: 'HomeCtrl',
+        // controller: 'StripeCtrl'
+
+    });
+}])
+.controller('HomeCtrl', ['$scope', '$firebaseAuth', function($scope, $firebaseAuth){
+
+    $scope.signIn = function(){
+        var username = $scope.user.email;
+        var password = $scope.user.password;
+        var authinticated = false;
+        var auth = $firebaseAuth();
+        auth.$signInWithEmailAndPassword(username,password).then(function(){
+            
+            console.log("User Login Success");
+            $scope.errMsg = false;
+        }).catch(function(error){
+            $scope.errMsg = true;
+            $scope.errorMessage = error.message
+        });
+    }
+     
+}])
+.controller('StripeCtrl',function($scope){
+    $scope.stripePay=function () {
+        var stripe = Stripe('pk_test_fseQvgioTBYVkqdlm4lTlczl');
+        var paymentRequest = stripe.paymentRequest({
+            country: 'US',
+            currency: 'usd',
+            total: {
+                label: 'Demo total',
+                amount: 1000,
+            },
+        });
+        var elements = stripe.elements();
+        var prButton = elements.create('paymentRequestButton', {
+            paymentRequest: paymentRequest,
+        });
+
+        // Check the availability of the Payment Request API first.
+        paymentRequest.canMakePayment().then(function (result) {
+            if (result) {
+                console.log("test1");
+                prButton.mount('#payment-request-button');
+                paymentRequest.on('token', function (ev) {
+
+                    // Send the token to your server to charge it!
+                    fetch('/charges', {
+                        method: 'POST',
+                        body: JSON.stringify({ token: ev.token.id }),
+                        headers: { 'content-type': 'application/json' },
+                    })
+                        .then(function (response) {
+                            console.log(response);
+                            if (response.ok) {
+                                console.log("succes");
+                                // Report to the browser that the payment was successful, prompting
+                                // it to close the browser payment interface.
+                                ev.complete('success');
+                            } else {
+                                console.log("failure");
+                                // Report to the browser that the payment failed, prompting it to
+                                // re-show the payment interface, or show an error message and close
+                                // the payment interface.
+                                ev.complete('fail');
+                            }
+                        });
+                });
+            } else {
+                console.log("test2");
+                document.getElementById('payment-request-button').style.display = 'none';
+                
+            }
+        });
+        
+    }
+    
+
+}
+)
+
---
+++ app/stripeForm/stripeForm.js
@@ -0,0 +1,83 @@
+'use restrict';
+
+angular.module('webApp.stripeForm', ['ngRoute', 'firebase'])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/stripeForm', {
+      templateUrl: '/stripeForm/stripeForm.html',
+      controller: 'StripeCtrl'
+    });
+  }])
+  .controller('StripeCtrl', ['$scope', '$firebaseAuth', '$http', function ($scope, $firebaseAuth, $http) {
+    var card;
+    var stripe = Stripe('pk_test_J0wFOYMrJQQsLv874nTFU9AT');
+    $scope.init = function () {
+      var elements = stripe.elements({
+        fonts: [
+          {
+            cssSrc: 'https://fonts.googleapis.com/css?family=Roboto',
+          },
+        ],
+        // Stripe's examples are localized to specific languages, but if
+        // you wish to have Elements automatically detect your user's locale,
+        // use `locale: 'auto'` instead.
+        locale: window.__exampleLocale
+      });
+
+      card = elements.create('card', {
+        iconStyle: 'solid',
+        style: {
+          base: {
+            iconColor: '#c4f0ff',
+            color: '#fff',
+            fontWeight: 500,
+            fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
+            fontSize: '16px',
+            fontSmoothing: 'antialiased',
+
+            ':-webkit-autofill': {
+              color: '#fce883',
+            },
+            '::placeholder': {
+              color: '#87BBFD',
+            },
+          },
+          invalid: {
+            iconColor: '#FFC7EE',
+            color: '#FFC7EE',
+          },
+        },
+      });
+      card.mount('#example1-card');
+    }
+    $scope.stripePay = function () {
+      // Create a token or display an error when the form is submitted.
+      var form = document.getElementById('payment-form');
+      form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        stripe.createToken(card).then(function (result) {
+          if (result.error) {
+            // Inform the customer that there was an error.
+
+          } else {
+            console.log(result.token)
+            // Simple GET request example:
+            $http({
+              method: 'POST',
+              url: 'https://3115a353.ngrok.io/charge',
+              data: { token: result.token }
+            }).then(function successCallback(response) {
+              console.log("DONE MOFOS")
+              console.log(JSON.stringify(response));
+            }, function errorCallback(response) {
+              console.log("NOT DONE MOFOS")
+              console.log(JSON.stringify(response));
+            });
+          }
+        });
+      });
+    }
+    $scope.init();
+  }
+  ]
+  )
